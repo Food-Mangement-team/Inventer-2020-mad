d3739e4c96b026d305fa9b109ff95da1b4e3da3c
Varani1998 <68747575+Varani1998@users.noreply.github.com>
2020-10-02 13:53:43 -0700

    balla

---
+++ app/src/main/java/com/example/mad_2020_inventers/OrderItemListClass.java
@@ -0,0 +1,43 @@
+package com.example.mad_2020_inventers;
+
+public class OrderItemListClass {
+    String ItemName;
+    int Quantity;
+    String paymentMethod;
+
+    public OrderItemListClass(String ItemName,int Quantity,String paymentMethod) {
+        this.ItemName=ItemName;
+        this.Quantity=Quantity;
+        this.paymentMethod=paymentMethod;
+    }
+
+    public OrderItemListClass() {
+
+    }
+
+    public Integer getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        Quantity = quantity;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String itemName) {
+        ItemName = itemName;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
+
+
